12a631cfb05e11c64e69b92621dce8f1f769b8cc
phob0s <git@phob0s.pl>
2020-04-07 18:28:32 +0200

    owm: add timed caching

--- go.mod
+++ go.mod
@@ -4,5 +4,6 @@ go 1.14
 
 require (
 	github.com/99designs/gqlgen v0.11.3
+	github.com/patrickmn/go-cache v2.1.0+incompatible
 	github.com/vektah/gqlparser v1.3.1
 )
---
+++ owm/owm.go
@@ -0,0 +1,46 @@
+package owm
+
+import (
+	"time"
+)
+
+const (
+	apiUrl = ""
+)
+
+type OpenWeatherMap struct {
+	// cityCache contains cached result of cities
+	// The cache data will expire in 10 minutes
+	// ref: https://openweathermap.org/appid#work
+	cityCache *cache.Cache
+
+	// token is openweathermap.org access token
+	token string
+}
+
+func New(token string) *OpenWeatherMap {
+	return &OpenWeatherMap{
+		cityCache: cache.New(10*time.Minute, 10*time.Minute),
+		token:     token,
+	}
+}
+
+func (o *OpenWeatherMap) City(name string) ([]byte, error) {
+	result, exist := o.cityCache.Get(name)
+	if exist {
+		val, ok := result.([]byte)
+		if ok {
+			return val, nil
+		}
+	}
+
+	return nil, nil
+}
+
+func (o *OpenWeatherMap) apiCall(url string) ([]byte, error) {
+	return nil, nil
+}
+
+func (o *OpenWeatherMap) lang() {
+
+}
